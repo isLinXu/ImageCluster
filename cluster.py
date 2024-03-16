185db89bfbd3a13bc93d6e4b41558a789f3eec05
isLinXu <2267379130@qq.com>
2024-03-16 10:23:30 +0800

    update cluster.py

--- cluster.py
+++ cluster.py
@@ -7,6 +7,7 @@ import torchvision.transforms as transforms
 from PIL import Image
 from clip import clip
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 import hashlib
@@ -15,6 +16,7 @@ from tqdm import tqdm
 
 class ImageCluster:
     def __init__(self, image_folder, text, model_name="ViT-B/32", device=None):
+        self.image_list = None
         self.image_folder = image_folder
         self.text = text
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
@@ -33,42 +35,77 @@ class ImageCluster:
             text_features = self.model.encode_text(text_input)
 
         for image_name in tqdm(os.listdir(self.image_folder)):
-        # for image_name in os.listdir(self.image_folder):
             image_path = os.path.join(self.image_folder, image_name)
-            image = Image.open(image_path)
-            image_input = self.preprocess(image).unsqueeze(0).to(self.device)
-            # 计算MD5哈希值并检查是否已存在
-            image_hash = self._compute_md5(image_path)
-            if image_hash in image_hashes:
-                continue
-            image_hashes.add(image_hash)
-            with torch.no_grad():
-                features = self.model.encode_image(image_input)
-                # Compute the similarity between the image and text features
-                similarity = torch.nn.functional.cosine_similarity(features, text_features)
-                # Expand the similarity tensor to 2D
-                similarity = similarity.unsqueeze(-1)
-                # Concatenate the features and similarity
-                features_with_similarity = torch.cat([features, similarity], dim=-1)
+            try:
+                image = Image.open(image_path)
+                image_input = self.preprocess(image).unsqueeze(0).to(self.device)
+                # Compute the MD5 hash and check if it already exists
+                image_hash = self._compute_md5(image_path)
+                if image_hash in image_hashes:
+                    continue
+                image_hashes.add(image_hash)
+                with torch.no_grad():
+                    features = self.model.encode_image(image_input)
+                    # Compute the similarity between the image and text features
+                    similarity = torch.nn.functional.cosine_similarity(features, text_features)
+                    # Expand the similarity tensor to 2D
+                    similarity = similarity.unsqueeze(-1)
+                    # Concatenate the features and similarity
+                    features_with_similarity = torch.cat([features, similarity], dim=-1)
 
-            # print(similarity.item())
-            # Filter out images with similarity below the threshold
-            if similarity.item() > self.similarity_threshold:
-                image_features.append(features_with_similarity.cpu().numpy())
-                image_list.append(image_name)
+                # Filter out images with similarity below the threshold
+                if similarity.item() > self.similarity_threshold:
+                    image_features.append(features_with_similarity.cpu().numpy())
+                    image_list.append(image_name)
+            except Exception as e:
+                print(f"Error processing image {image_name}: {e}")
 
         return np.vstack(image_features), image_list
 
-    def cluster_images(self, n_clusters=2):
+    def set_similarity_threshold(self, threshold):
+        self.similarity_threshold = threshold
+
+    def get_image_list(self):
+        return os.listdir(self.image_folder)
+
+    def get_image_count(self):
+        return len(os.listdir(self.image_folder))
+    def cluster_images(self, n_clusters=2, filter_similarity=True):
         image_features, image_list = self._extract_features()
         kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(image_features)
 
         clusters = {i: [] for i in range(n_clusters)}
-        for image_name, label in zip(image_list, kmeans.labels_):
+        cluster_features = {i: [] for i in range(n_clusters)}
+
+        for image_name, label, features in zip(image_list, kmeans.labels_, image_features):
             clusters[label].append(image_name)
+            cluster_features[label].append(features)
+
+        if filter_similarity:
+            # Compute the mean similarity for each cluster
+            cluster_mean_similarities = {}
+            for label, features in cluster_features.items():
+                mean_similarity = self._compute_cluster_mean_similarity(features)
+                cluster_mean_similarities[label] = mean_similarity
+
+            # Filter out images with similarity below the threshold
+            filtered_clusters = {}
+            for label, images in clusters.items():
+                mean_similarity = cluster_mean_similarities[label]
+                filtered_images = [image_name for image_name, features in zip(images, cluster_features[label]) if
+                                   features[-1] >= mean_similarity]
+                filtered_clusters[label] = filtered_images
+
+            clusters = filtered_clusters
 
         silhouette_avg = silhouette_score(image_features, kmeans.labels_)
         return clusters, silhouette_avg
+    def _compute_cluster_mean_similarity(self, cluster_features):
+        similarities = []
+        for features in cluster_features:
+            similarity = features[-1]
+            similarities.append(similarity)
+        return np.mean(similarities)
 
     def print_clusters(self, clusters):
         for i, images in clusters.items():
@@ -107,20 +144,48 @@ class ImageCluster:
             md5_hash = hashlib.md5(file_data).hexdigest()
         return md5_hash
 
-    # 输出聚类中的图片数量
     def print_cluster_counts(self, clusters):
         for i, images in clusters.items():
             print(f"Cluster {i + 1}: {len(images)} images")
 
+    def find_optimal_clusters(self, min_clusters=2, max_clusters=10):
+        image_features, _ = self._extract_features()
+        n_samples = image_features.shape[0]
+        max_clusters = min(max_clusters, n_samples - 1)
+
+        silhouette_avgs = []
+        for n_clusters in range(min_clusters, max_clusters + 1):
+            kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(image_features)
+            silhouette_avg = silhouette_score(image_features, kmeans.labels_)
+            silhouette_avgs.append(silhouette_avg)
+
+        optimal_clusters = silhouette_avgs.index(max(silhouette_avgs)) + min_clusters
+        return optimal_clusters
+
+    def visualize_clusters(self, clusters):
+        image_features, _ = self._extract_features()
+        pca = PCA(n_components=2)
+        reduced_features = pca.fit_transform(image_features)
+
+        for cluster_id, image_list in clusters.items():
+            cluster_features = reduced_features[[image_list.index(image_name) for image_name in image_list]]
+            plt.scatter(cluster_features[:, 0], cluster_features[:, 1], label=f'Cluster {cluster_id + 1}')
+        plt.legend()
+        plt.show()
+
+
+
 if __name__ == "__main__":
-    image_folder = "/Users/gatilin/youtu-work/SVAP/合景悠活项目/test_img_split_point_data_0304/images"
-    dest_folder = "/Users/gatilin/youtu-work/SVAP/合景悠活项目/test_img_split_point_data_0304/cluster"  # 目的文件夹
+    image_folder = "/Users/gatilin/test_img_split_point_data_0304/images"
+    dest_folder = "/Users/gatilin/test_img_split_point_data_0304/cluster7"  # 目的文件夹
+
     text = "This is a scene of a outdoor"
     image_cluster = ImageCluster(image_folder, text)
-    n_clusters = 10
+    n_clusters = image_cluster.find_optimal_clusters(min_clusters=2, max_clusters=15)
+    print(f"Optimal number of clusters: {n_clusters}")
     clusters, silhouette_avg = image_cluster.cluster_images(n_clusters=n_clusters)
     print(f"Silhouette Score: {silhouette_avg:.2f}")
     image_cluster.print_clusters(clusters)
     image_cluster.print_cluster_counts(clusters)
     image_cluster.save_clusters(clusters, dest_folder)  # 保存聚类结果
-    image_cluster.plot_silhouette_scores(max_clusters=10)
+    image_cluster.visualize_clusters(clusters)
